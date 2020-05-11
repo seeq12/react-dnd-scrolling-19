ac469d46f42be30a7d6aa81b02d2981f485379d2
Wilson Tian <wilson.tian@perkinelmer.com>
2020-05-11 16:41:41 +0800

    update examples

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContextProvider } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import withScrolling from 'react-dnd-scrolling';
 import DragItem from './DragItem';
 import './App.css';
@@ -12,13 +12,13 @@ const ITEMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export default class App extends Component {
   render() {
     return (
-      <DragDropContextProvider backend={HTML5Backend}>
+      <DndProvider backend={HTML5Backend}>
         <ScrollingComponent className="App">
           {ITEMS.map(n => (
             <DragItem key={n} label={`Item ${n}`} />
           ))}
         </ScrollingComponent>
-      </DragDropContextProvider>
+      </DndProvider>
     );
   }
 }
